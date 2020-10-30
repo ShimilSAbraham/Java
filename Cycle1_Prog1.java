924add44c52086b93097f24a8616a2d4d8d91dcd
Shimil Shaji Abraham <62107737+ShimilSAbraham@users.noreply.github.com>
2020-10-30 13:54:33 +0530

    Created first program of cycle 1

---
+++ Cycle1_Prog1.java
@@ -0,0 +1,9 @@
+// First program
+public class Example
+{
+    public static void main(String args[])
+    {
+        //prints the statement "This is a sample program"
+        System.out.println("This is a sample program");
+    }
+}
