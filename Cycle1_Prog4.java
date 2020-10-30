3aa8f622a4e301c90050a9d2fd6706c62d706c6b
Shimil Shaji Abraham <62107737+ShimilSAbraham@users.noreply.github.com>
2020-10-30 13:58:40 +0530

    Created program 4 of cycle 1

---
+++ Cycle1_Prog4.java
@@ -0,0 +1,13 @@
+//to print the cubes of first ten numbers using a for loop
+public class Example4
+{
+    public static void main(String args[])
+    {
+        //for loop starts here
+        for(int i=1; i<=10; i++)
+        {
+            //this is the body of the loop
+            System.out.println(i*i*i);
+        }
+    }
+}
