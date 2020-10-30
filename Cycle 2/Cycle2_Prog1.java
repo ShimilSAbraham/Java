ba8e80ddedd5da8671e212d1ac0b2a5b1cac6910
Shimil Shaji Abraham <62107737+ShimilSAbraham@users.noreply.github.com>
2020-10-30 14:07:46 +0530

    Create Cycle2_Prog1.java

---
+++ Cycle 2/Cycle2_Prog1.java
@@ -0,0 +1,14 @@
+//displaying a 1D array
+public class Demo2_1
+{
+   public static void main(String args[])
+   {
+       System.out.println(" ");
+       int a[] = new int[5];
+       int i;
+       for(i=0;i<5;i++)
+       a[i] = i;
+       for(i=0;i<5;i++)
+       System.out.println(a[i] + " ");
+    }
+}
