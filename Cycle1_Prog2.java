23f819bd1df9c09ae8411a840a723a51479ab19d
Shimil Shaji Abraham <62107737+ShimilSAbraham@users.noreply.github.com>
2020-10-30 13:55:53 +0530

    Created program 2 of cycle 1

---
+++ Cycle1_Prog2.java
@@ -0,0 +1,11 @@
+/* second program to demonstrate the use of variable declaration */
+public class Example2
+{
+    public static void main(String args[])
+    {
+        int num = 25; //to declare a variable 'num' and assign an integer value of 25
+        System.out.println("The number is "+ num);
+        num *= 3;   //to change the value of num to three times itself
+        System.out.println("Three times of the number is "+ num);
+    }
+}
