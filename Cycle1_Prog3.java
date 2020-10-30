e281a10711f14891fa176c3d920e82c224ebe59e
Shimil Shaji Abraham <62107737+ShimilSAbraham@users.noreply.github.com>
2020-10-30 13:57:34 +0530

    Created program 3 of cycle 1

---
+++ Cycle1_Prog3.java
@@ -0,0 +1,19 @@
+/* this program shows the working of an 'if' statement
+   by showing whether a number is positive, negative or zero
+ */
+public class Example3
+{
+    public static void main(String args[])
+    {
+        int num = -34;
+        //to check if number is positive
+        if(num>0)
+        System.out.println("The number "+ num +" is positive");
+        //to check if number is zero
+        if(num==0)
+        System.out.println("The number "+ num +" is zero");
+        //to check if number is negative
+        if(num<0)
+        System.out.println("The number "+ num +" is negative");
+    }
+}
